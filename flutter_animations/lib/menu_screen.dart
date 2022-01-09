cb77a2e541b4ed92981cfe0db83d4580cee56e38
Lucas Gabriel <lucasbsb100@gmail.com>
2022-01-09 18:18:12 -0300

    Add menu screen with animations options

---
+++ flutter_animations/lib/menu_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations/heart_screen.dart';
+import 'package:flutter_animations/star_spinning.dart';
+import 'package:rive/rive.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Menu"),
+        centerTitle: true,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+        padding: EdgeInsets.all(8.0),
+        children: [
+          GestureDetector(
+            onTap: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => HeartScreen()));
+
+            },
+            child: Container(
+              color: Colors.white,
+              child: RiveAnimation.asset('assets/heart_animations.riv',),
+            ),
+          ),
+          GestureDetector(
+            onTap: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => StarSpinning()));
+            },
+            child: Container(
+              color: Colors.blue[200],
+              child: RiveAnimation.asset('assets/star_spinning.riv', animations: ['Spinning']),
+            ),
+          ),
+          Container(
+            child: RiveAnimation.asset('assets/tree_demo.riv', animations: ['Example'],),
+          ),
+        ],
+      ),
+    );
+  }
+}
